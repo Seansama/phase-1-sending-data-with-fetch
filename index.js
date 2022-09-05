680431475f28a8848d45a6b55acbec0e8a775f6e
Shaun Mwangi <shaun.mwangi@student.moringaschool.com>
2022-09-05 16:50:03 +0100

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Add your code here
+function submitData(name, email) {
+    console.log('here')
+    const config = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name,
+            email,
+        })
+    }
+   
+    fetch('http://localhost:3000/users', config)
+      .then(function (response) {
+            if(!response.ok){
+                const error = new Error('Unauthorized Access');
+                error.code = response.status;
+                throw error;
+            }
+            return response.json();
+        })
+
+        .then(function (data) {
+            const createdP = document.createElement('p');
+            const body = document.body;
+            createdP.textContent = `${data.id}`;
+            body.appendChild(createdP);
+            
+        }).catch((error) => {
+            const createdHeader = document.createElement('p');
+            const body = document.body;
+            createdHeader.textContent = error.error.message;
+            body.appendChild(createdHeader);
+        })
+}
+
+submitData('Steve', 'steve@steve.com');
